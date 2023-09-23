0f2e60cdfbe50de093c22f3fca4b63617c48b29e
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-09-23 13:23:06 +1000

    Add get score function

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,13 +1,28 @@
 """
 CP1404- Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    score_category = get_score(score)
+    random_score = random.randint(0, 100)
+    random_score_category = get_score(random_score)
+    print(f"User Score: {score} \nScore Category: {score_category}")
+    print(f"Random Score: {random_score} \nRandom Score Category: {random_score_category}")
+
+
+def get_score(score):
+    """Will return score category"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Pass"
+    else:
+        return "Bad"
+
+
+main()
