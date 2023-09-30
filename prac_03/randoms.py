d4d19a37284edf2f027ad4053540112be911240c
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-09-30 13:01:08 +1000

    Add random numbers program

---
+++ prac_03/randoms.py
@@ -0,0 +1,30 @@
+"""
+What did you see on line 1?
+on line 1 the number 16 is shown
+
+What was the smallest number you could have seen, what was the largest?
+The smallest number would be 5 and the largest would be 19
+
+What did you see on line 2?
+On line 2 the number 5 is shown
+
+What was the smallest number you could have seen, what was the largest?
+The smallest number would be 3 and the largest number would be 9
+
+Could line 2 have produced a 4?
+No because it steps every 2 digits
+
+What did you see on line 3?
+On line 3 the number 4.84 is shown
+
+What was the smallest number you could have seen, what was the largest?
+The smallest number would be 2.5 and the largest would be 5.499999999999999
+
+
+Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+"""
+import random
+
+random_number = random.randint(2, 100)
+print(random_number)
+
