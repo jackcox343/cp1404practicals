fbf3b21217830a515314bdac09fc13717a3783f7
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-10-31 23:00:05 +1000

    add testing to program

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,19 @@
+"""
+Guitar test
+Estimate: 30 mins
+Actual:
+"""
+from prac_06.guitar import Guitar
+
+CURRENT_YEAR = 2023
+
+guitar_one = Guitar("Gibson L-5 CES", 1992, 16035.40)
+guitar_one_age = guitar_one.get_age(CURRENT_YEAR)
+another_guitar = Guitar("Another Guitar", 2013, 200)
+print(guitar_one)
+print(another_guitar)
+
+print(f"{guitar_one} get_age() - Expected 100. Got {guitar_one_age}")
+print(f"{another_guitar} get_age() - Expected 9. Got {another_guitar.get_age(CURRENT_YEAR)}")
+print(f"{guitar_one} is_vintage() - Expected True. Got {guitar_one.is_vintage(CURRENT_YEAR)}")
+print(f"{another_guitar} is_vintage() - Expected False. Got {another_guitar.is_vintage(CURRENT_YEAR)}")
