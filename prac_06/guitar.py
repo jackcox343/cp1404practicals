5369c0a4389064da78a93d050fce5e20b5c9008c
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-10-31 22:14:42 +1000

    add get age method to program

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -6,6 +6,8 @@ Actual:
 
 
 class Guitar:
+    """Class for guitar to store guitars"""
+
     def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
@@ -14,4 +16,5 @@ class Guitar:
     def __str__(self):
         return f"{self.name} {self.year} : {self.cost}"
 
-
+    def get_age(self,current_year=2023):
+        return f"In 2023 the {self.name} is: {current_year - self.year} years old"
