ca30d722d29f5ec7b5ae38b77827e4b2df5a2d36
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-10-31 20:50:14 +1000

    add list and for loops to program

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -3,3 +3,15 @@ languages
 Estimate: 50 mins
 Actual:
 """
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
+
+programming_languages = [python, visual_basic, ruby]
+print("The dynamically typed languages are:")
+for language in programming_languages:
+    if language.is_dynamic():
+        print(language.field)
