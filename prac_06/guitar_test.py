ba6adf6463bffdf281824ad02381f31b2f4b092f
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-10-31 23:04:20 +1000

    add doc strings to program

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -6,12 +6,13 @@ Actual:
 from prac_06.guitar import Guitar
 
 CURRENT_YEAR = 2023
-
+"""Testing for Guitar class"""
 guitar_one = Guitar("Gibson L-5 CES", 1992, 16035.40)
 guitar_one_age = guitar_one.get_age(CURRENT_YEAR)
 another_guitar = Guitar("Another Guitar", 2013, 200)
 print(guitar_one)
 print(another_guitar)
+
 
 print(f"{guitar_one} get_age() - Expected 100. Got {guitar_one_age}")
 print(f"{another_guitar} get_age() - Expected 9. Got {another_guitar.get_age(CURRENT_YEAR)}")
