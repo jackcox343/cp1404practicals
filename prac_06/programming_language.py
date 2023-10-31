eff4642c038af35d4518b22a32ee913b33c41179
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-10-31 20:21:52 +1000

    add doc strings to program

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -6,13 +6,15 @@ Actual:
 
 
 class ProgrammingLanguage:
-    """"""
+    """Programming class"""
 
     def __init__(self, field="", typing="", reflection=True, year=0):
+        """Initialises language instances"""
         self.field = field
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
+        """Returns string based of programming languages"""
         return f"{self.field}, {self.typing} Typing, Reflection={self.reflection}, First appeared in 1999"
