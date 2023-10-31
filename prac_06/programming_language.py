2ecd2622ef52885300c3ba7d749b3bce31eb099e
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-10-31 19:55:42 +1000

    add initialiser to program

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -3,3 +3,14 @@ Programming language
 Estimate: 30 mins
 Actual:
 """
+
+
+class ProgrammingLanguage:
+    """"""
+
+    def __init__(self, field="", typing="", reflection=True, year=0):
+        self.field = field
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
