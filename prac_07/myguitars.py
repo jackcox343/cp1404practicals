82637245d8bfffed3c070cba782826e775a6270e
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-11-05 23:11:41 +1000

    add read guitars and display guitars function to program

---
+++ prac_07/myguitars.py
@@ -0,0 +1,32 @@
+from prac_07.guitar import Guitar
+
+FILENAME = "guitars.csv"
+
+
+def main():
+    FILENAME = "guitars.csv"
+    guitars = read_guitars(FILENAME)
+    display_guitars(guitars)
+
+
+def read_guitars(FILENAME):
+    guitars = []
+    with open(FILENAME, "r") as file:
+        for line in file:
+            parts = line.strip().split(",")
+            if len(parts) == 3:
+                name, year, cost = parts
+                year = int(year)
+                cost = float(cost)
+                guitar = Guitar(name, year, cost)
+                guitars.append(guitar)
+    file.close()
+    return guitars
+
+
+def display_guitars(guitars):
+    for guitar in guitars:
+        print(guitar)
+
+
+main()
