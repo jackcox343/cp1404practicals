111903e432f32a58e1894b40923d25e0dfe714d8
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-11-06 21:21:16 +1000

    adds project class to program

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,3 +3,13 @@ Project
 Estimate: 60 mins
 Actual:
 """
+
+
+class Project:
+    def __init__(self, name="", start_date=0, end_date=0, priority=0.0, completion=0):
+        self.name = name
+        self.start_date = start_date
+        self.end_date = end_date
+        self.priority = priority
+        self.completion = completion
+
