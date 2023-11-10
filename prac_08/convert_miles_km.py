d669176c3002ad927880851528415380a804e264
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-11-10 17:21:02 +1000

    adds calculate function to program

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -8,14 +8,17 @@ from kivy.lang import Builder
 
 MILES_TO_KILOMETRES = 1.60934
 
+
 class ConvertMilesApp(App):
     def build(self):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
 
-
-
+    def handle_calculate(self):
+        value = int(input())
+        result = value * MILES_TO_KILOMETRES
+        self.root.ids.output_label.text = str(result)
 
 
 ConvertMilesApp().run()
