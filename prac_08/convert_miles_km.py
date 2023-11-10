78d284609cf832916ff5e6e7c5be7723979e306c
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-11-10 21:03:39 +1000

    adds doc strings to program

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,7 +1,7 @@
 """
 Convert miles program
 Estimate: 60 mins
-Actual:
+Actual: 60 mins
 """
 from kivy.app import App
 from kivy.lang import Builder
@@ -10,24 +10,30 @@ MILES_TO_KILOMETRES = 1.60934
 
 
 class ConvertMilesApp(App):
+    """This a class for miles conversion app"""
+
     def build(self):
+        """This function builds loaded file"""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
 
     def handle_calculate(self):
+        """This function handles miles to km calculation"""
         value = self.get_valid_miles()
         result = value * MILES_TO_KILOMETRES
         self.root.ids.output_label.text = str(result)
 
     def get_valid_miles(self):
+        """This function gets valid input"""
         try:
             value = float(self.root.ids.input_miles.text)
             return value
         except ValueError:
             return 0
 
-    def handle_increment(self,change):
+    def handle_increment(self, change):
+        """This function handles incrementing for button press"""
         value = self.get_valid_miles() + change
         self.root.ids.input_miles.text = str(value)
         self.handle_calculate()
