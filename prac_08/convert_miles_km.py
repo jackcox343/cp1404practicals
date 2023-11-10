b974b07044b0e6d5e58f2a963ff0f6f2cfd3793f
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-11-10 16:42:55 +1000

    adds boxlayout and conversion class to program

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -3,3 +3,19 @@ Convert miles program
 Estimate: 60 mins
 Actual:
 """
+from kivy.app import App
+from kivy.lang import Builder
+
+MILES_TO_KILOMETRES = 1.60934
+
+class ConvertMilesApp(App):
+    def build(self):
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file("convert_miles_km.kv")
+        return self.root
+
+
+
+
+
+ConvertMilesApp().run()
