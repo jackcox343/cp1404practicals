2c753fcb2514296ceaa5e4453937a860abb5cb82
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-11-10 21:47:18 +1000

    adds doc strings to program

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,23 +1,28 @@
 """
 Box layout demo
-Estimate time: 30
-Actual time: 30
+Estimate time: 30 mins
+Actual time: 30 mins
 """
 from kivy.app import App
 from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """This is the class for a boxlayout demo app"""
+
     def build(self):
+        """This function load file"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """This function handles greets"""
         print("test")
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """This function handles clearing of text"""
         print("test")
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
