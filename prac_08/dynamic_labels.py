a66ef9a722d8d66e076531c9b5f55c8e2ad99fd8
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-11-11 20:36:53 +1000

    adds doc strings to complete program

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -1,5 +1,33 @@
 """
 Dynamic labels
 Estimate: 60 mins
-Actual:
+Actual: 60 mins
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabel(App):
+    """Dynamic labeling class"""
+    def __init__(self, **kwargs):
+        """This function constructs main app"""
+        super().__init__(**kwargs)
+        self.names = ["Jack", "John", "Alice", "Jennifer"]
+
+    def build(self):
+        """This function loads file"""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file("dynamic_labels.kv")
+        self.add_labels(self.names)
+        return self.root
+
+    def add_labels(self, names):
+        """This function adds names to labels"""
+        for name in names:
+            label = Label(text=name)
+            self.root.ids.main.add_widget(label)
+
+
+DynamicLabel().run()
