f35ed45d7ea7359282e96667e5a1bef70cf9d4ae
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-11-18 20:07:03 +1000

    add choose taxi function to taxi simulator program

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -3,19 +3,41 @@ Taxi Simulator
 Estimate: 60 mins
 Actual:
 """
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
 
 
 def main():
+    current_taxi = None
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
     choice = input("q)uit, c)hoose taxi, d)rive\n>>> ").lower()
     while choice != "q":
         if choice == "c":
-            pass
+            current_taxi = choose_taxi(taxis)
         elif choice == "d":
-            pass
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
+            else:
+                pass
         else:
             print("Invalid option")
             choice = input("q)uit, c)hoose taxi, d)rive\n>>> ").lower()
+
+
+def choose_taxi(taxis):
+    print(f"Taxis available:\n{display_taxi(taxis)} ")
+    taxi_choice = input("Choose taxi: ")
+    try:
+        taxi_choice = int(taxi_choice)
+        if taxi_choice < len(taxis):
+            return taxis[taxi_choice]
+        else:
+            print("Invalid taxi choice")
+            return None
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return None
 
 
 def display_taxi(taxis):
