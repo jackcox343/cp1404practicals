e1916eb38140f2ed95eb87be9f65d95ab40bc8f2
Masenk0 <87218555+Masenk0@users.noreply.github.com>
2023-11-18 22:45:22 +1000

    add display taxi function to taxi simulator program

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -9,24 +9,30 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 
 def main():
     current_taxi = None
+    bill_to_date = 0.0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
     choice = input("q)uit, c)hoose taxi, d)rive\n>>> ").lower()
     while choice != "q":
         if choice == "c":
+            print(f"Taxis available:")
+            display_taxi(taxis)
             current_taxi = choose_taxi(taxis)
         elif choice == "d":
             if current_taxi is None:
                 print("You need to choose a taxi before you can drive")
             else:
-                pass
+                distance = float(input("Drive how far? "))
+                cost = current_taxi.drive(distance)
+                bill_to_date += cost
+                print(f"Your {current_taxi.name} trip cost you ${cost:.2f}")
         else:
             print("Invalid option")
             choice = input("q)uit, c)hoose taxi, d)rive\n>>> ").lower()
 
 
 def choose_taxi(taxis):
-    print(f"Taxis available:\n{display_taxi(taxis)} ")
+
     taxi_choice = input("Choose taxi: ")
     try:
         taxi_choice = int(taxi_choice)
